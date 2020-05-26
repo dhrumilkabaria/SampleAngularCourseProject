e8b6aa45cf7382646c4a3a84e799862b808a34ae
Dhrumil Kabaria <dhrumilkabaria1200@gmail.com>
2020-05-27 01:32:17 +0530

    course project

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,8 @@
+export class Ingredient {
+    // public name: string;
+    // public amount: number;
+    // we can directly write access specifier in the constructor argument itself and then no need to declare data memebr in the class.
+    constructor(public name: string, public amount: number) {
+
+    }
+}
